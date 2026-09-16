eeda40b48410874146ab46b2596a01521d2da60e
Ridwan <ridwanraish2003@gmail.com>
2026-09-16 11:45:58 +0600

    removed unneccessary files

--- backend/src/routes/reportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateReport } = require('../controllers/reportController');
-
-router.get('/', generateReport);
-
-module.exports = router;
